46daea765e80c63639d55c3d39f5d453e8848e49
ManuxDark <mgcastelberg@hotmail.com>
2025-02-27 21:45:28 -0600

    Context y Reducer de mi aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { AppRouter } from './router/AppRouter'
+import { AuthProvider } from './auth'
 
 const App = () => {
   return (
-    <>
+    <AuthProvider>
       <AppRouter />
-    </>
+    </AuthProvider>
   )
 }
 
---
+++ src/auth/types/types.js
@@ -0,0 +1,8 @@
+
+
+
+// Nombres de los tipos de las acciones
+export const types = {
+    login: '[Auth] Login',
+    logout: '[Auth] Logout'
+}
