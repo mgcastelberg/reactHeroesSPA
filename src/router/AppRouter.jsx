b16893c871322b7487b2fd9cd637729735441a00
ManuxDark <mgcastelberg@hotmail.com>
2025-03-04 00:05:25 -0600

    Rutas privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { HeroesRoutes } from "../heroes";
 import { LoginPage } from "../auth/pages/LoginPage";
-import { Navbar } from "../ui";
+import { PrivateRoute } from "./PrivateRoute";
 
 
 // Version React Router v6
-export const AppRouter = () => {
+export const AppRouter= () => {
   return (
     <>
       <Routes>
         <Route path="login" element={<LoginPage />} />
-        <Route path="/*" element={<HeroesRoutes />} />
-        {/* <Route path="/" element={<LoginPage />} /> */}
+
+        <Route path="/*" element={
+          <PrivateRoute >
+            <HeroesRoutes />
+          </PrivateRoute>
+        } />
+
+        {/* <Route path="/*" element={<HeroesRoutes />} /> */}
+
       </Routes>
     </>
   );
